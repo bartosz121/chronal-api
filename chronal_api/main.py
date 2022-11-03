ba1a7ebc20f399ea3405e860bda6479446aca482
bartosz121 <bmagiera121@gmail.com>
2022-11-03 20:58:18 +0100

    Run alembic on startup, rename config

--- chronal_api/main.py
+++ chronal_api/main.py
@@ -1,28 +1,32 @@
 import uvicorn
 import logging
+from pkg_resources import resource_filename
 
+from alembic.command import upgrade
+from alembic.config import Config
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from chronal_api.core import debug, config
+from chronal_api.core import debug
+from chronal_api.core.config import get_config
 
 
-api_config = config.get_config()
+config = get_config()
 
 app = FastAPI(
-    title=api_config.TITLE,
-    version=api_config.VERSION,
-    root_path=api_config.ROOT_PATH,
-    docs_url=None if api_config.DISABLE_DOCS else "/docs",
-    redoc_url=None if api_config.DISABLE_DOCS else "/redoc",
+    title=config.TITLE,
+    version=config.VERSION,
+    root_path=config.ROOT_PATH,
+    docs_url=None if config.DISABLE_DOCS else "/docs",
+    redoc_url=None if config.DISABLE_DOCS else "/redoc",
 )
 
-if api_config.IS_DEV:
+if config.IS_DEV:
     app.add_middleware(debug.TimerMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=api_config.ALLOWED_ORIGINS,
+    allow_origins=config.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -31,24 +35,30 @@ app.add_middleware(
 app.add_middleware(debug.RequestUUIDMiddleware)
 
 
+@app.on_event("startup")
+def run_migrations():
+    config = Config(resource_filename("chronal_api", "alembic.ini"))
+    upgrade(config, "head")
+
+
 @app.get("/")
 def home():
-    return {"msg123": api_config.TITLE}
+    return {"msg": config.TITLE}
 
 
 @app.get("/config")
 def show_config():
-    return api_config
+    return config
 
 
 def run():
     uvicorn.run(
         "chronal_api.main:app",
         log_config=debug.LOGGING_CONFIG,
-        reload=api_config.UVICORN_RELOAD,
-        workers=api_config.UVICORN_WORKERS,
-        host=api_config.UVICORN_HOST,
-        port=api_config.UVICORN_PORT,
+        reload=config.UVICORN_RELOAD,
+        workers=config.UVICORN_WORKERS,
+        host=config.UVICORN_HOST,
+        port=config.UVICORN_PORT,
     )
 
 
