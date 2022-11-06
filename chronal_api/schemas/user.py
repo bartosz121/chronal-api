77b886a8c7afe6c9797c78e790f8a771b38afe95
bartosz121 <bmagiera121@gmail.com>
2022-11-06 22:28:12 +0100

    Add timezone in Read/Create/Update and timestamps in Read

--- chronal_api/schemas/user.py
+++ chronal_api/schemas/user.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime
 
 from fastapi_users import schemas
 
@@ -6,13 +7,18 @@ from fastapi_users import schemas
 class UserRead(schemas.BaseUser[uuid.UUID]):
     first_name: str
     last_name: str
+    timezone: str
+    created_at: datetime
+    updated_at: datetime
 
 
 class UserCreate(schemas.BaseUserCreate):
     first_name: str
     last_name: str
+    timezone: str
 
 
 class UserUpdate(schemas.BaseUserUpdate):
     first_name: str
     last_name: str
+    timezone: str
