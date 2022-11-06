7789720510ae4df946a5abd368d1549bde488006
bartosz121 <bmagiera121@gmail.com>
2022-11-06 22:27:15 +0100

    Add timezone and timestamps inheritance in User

--- chronal_api/models/user.py
+++ chronal_api/models/user.py
@@ -1,9 +1,12 @@
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
 
-from chronal_api.db import chronal_psql
+from chronal_api.db.chronal_psql import Base
+from chronal_api.models import Timestamps
 
 
-class User(SQLAlchemyBaseUserTableUUID, chronal_psql.Base):
+class User(SQLAlchemyBaseUserTableUUID, Timestamps, Base):
     first_name: str = Column(String(255), nullable=False)
     last_name: str = Column(String(255), nullable=False)
+    timezone: str = Column(String(64), nullable=False)  # default: "Etc/UTC"
