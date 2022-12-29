1b6ba6dd8c143811bf30d031644408d1ccab5c61
bartosz121 <bmagiera121@gmail.com>
2022-12-29 22:44:45 +0100

    Fix imports

--- chronal_api/alembic/env.py
+++ chronal_api/alembic/env.py
@@ -1,18 +1,18 @@
 from alembic import context
 from sqlalchemy import create_engine
 
-
+from chronal_api.auth.models import User
+from chronal_api.calendar.models import Calendar
+from chronal_api.calendar_access.models import CalendarAccess, CalendarAccessRole
 from chronal_api.core.config import get_config
-from chronal_api.db import chronal_psql
-from chronal_api.models import User
-
+from chronal_api.core.db import Base
 
 config = get_config()
 
 engine = create_engine(config.postgres_uri_psycopg2)
 
 with engine.connect() as connection:
-    context.configure(connection, target_metadata=chronal_psql.Base.metadata)
+    context.configure(connection, target_metadata=Base.metadata)
 
     with context.begin_transaction():
         context.run_migrations()
