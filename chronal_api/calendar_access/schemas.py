43f52ff02a28627d49d9837ecb734dd4c4903f8d
bartosz121 <bmagiera121@gmail.com>
2023-01-01 19:24:22 +0100

    Add calendar access schemas.py

---
+++ chronal_api/calendar_access/schemas.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, validator
+
+from chronal_api.calendar_access.models import CalendarAccessRole
+from chronal_api.core.abc import UUID
+
+
+class BaseCalendarAccess(BaseModel):
+    class Config:
+        orm_mode = True
+
+
+class CalendarAccessRead(BaseCalendarAccess):
+    id: UUID
+    calendar_id: UUID
+    user_id: UUID
+    role: CalendarAccessRole
+    created_by: UUID
+    created_at: datetime
+    updated_at: datetime
+
+
+class CalendarAccessCreateApi(BaseCalendarAccess):
+    user_id: UUID
+    role: CalendarAccessRole
+
+
+class CalendarAccessCreateDb(BaseCalendarAccess):
+    calendar_id: UUID
+    user_id: UUID
+    role: CalendarAccessRole
+    created_by: UUID
+
+
+class CalendarAccessUpdate(BaseCalendarAccess):
+    calendar_id: Optional[UUID]
+    user_id: Optional[UUID]
