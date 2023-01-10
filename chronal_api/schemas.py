09b5f1100092baa6d94494404c116103470c2ab2
bartosz121 <bmagiera121@gmail.com>
2023-01-10 21:07:53 +0100

    #8 Add timezone and datetime validators

--- chronal_api/schemas.py
+++ chronal_api/schemas.py
@@ -1,5 +1,12 @@
 import orjson
+import pendulum
+from pendulum.date import Date
+from pendulum.datetime import DateTime
+from pendulum.duration import Duration
+from pendulum.time import Time
 from pydantic import BaseModel
+
+TIMEZONES = pendulum.timezones  # type: ignore
 
 
 def orjson_dumps(v, *, default):
@@ -21,3 +28,38 @@ class ResourceUrl(ORJSONModel):
 
 class ExceptionModel(ORJSONModel):
     detail: str
+
+
+# Reusable validators
+
+
+def is_timezone_invalid(cls, timezone: str) -> str:
+    if timezone not in TIMEZONES:
+        raise ValueError("Invalid timezone")
+    return timezone
+
+
+def is_datetime_in_the_past(cls, dt: DateTime) -> DateTime:
+    """
+    Assumes that `dt` is in UTC
+    """
+    now = pendulum.now(tz="Etc/UTC")
+    if now > dt:
+        raise ValueError(f"Time: {dt} is in the past")
+    return dt
+
+
+def set_dt_to_utc(cls, v: str) -> Date | Time | DateTime | Duration:
+    """
+    Used in EventCreate pre=True validator to set all incoming datetimes as utc
+
+    FIXME find better way to do this
+        - waiting for pydantic V2 with custom encoding/decoding
+    """
+    try:
+        dt = pendulum.parser.parse(v)
+        if isinstance(dt, DateTime):
+            dt = dt.in_tz("Etc/UTC")
+    except Exception:  # FIXME
+        raise ValueError(f"Could not parse as {v} date time")
+    return dt
