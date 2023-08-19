c30ee564375c3c42adbfb4ab9e9232cddca87645
bartosz121 <bmagiera121@gmail.com>
2023-08-19 22:16:26 +0200

    Add `sql_error_handler`

---
+++ chronal_api/lib/repository/utils.py
@@ -0,0 +1,25 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+
+from chronal_api.log import get_logger
+
+from .exceptions import ConflictError, RepositoryException
+
+logger = get_logger()
+
+
+@asynccontextmanager
+async def sql_error_handler() -> AsyncIterator[None]:
+    try:
+        yield
+    except IntegrityError as e:
+        logger.error(str(e))
+        raise ConflictError from e
+    except SQLAlchemyError as e:
+        logger.error(str(e))
+        raise RepositoryException("An exception occurred while executing a SQL statement.") from e
+    except AttributeError as e:
+        logger.error(str(e))
+        raise RepositoryException from e
