b05992e2d312918b140d28952ebd4dadbefafb37
bartosz121 <bmagiera121@gmail.com>
2023-09-20 21:41:05 +0200

    Move database code to /lib/database

--- chronal_api/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-
-from chronal_api.settings import DatabaseSettings
-
-db_settings = DatabaseSettings()
-
-engine = create_async_engine(db_settings.url, echo=db_settings.ECHO)
-
-sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
