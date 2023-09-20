eb3932e5ac66fcf8e860a01b5a93a02b9167f9c6
bartosz121 <bmagiera121@gmail.com>
2023-09-20 21:41:05 +0200

    Move database code to /lib/database

---
+++ chronal_api/lib/database/engine.py
@@ -0,0 +1,25 @@
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+from chronal_api.settings import DatabaseSettings
+
+DB_NAMING_CONVENTION = {
+    "ix": "%(column_0_label)s_idx",
+    "uq": "%(table_name)s_%(column_0_name)s_key",
+    "ck": "%(table_name)s_%(constraint_name)s_check",
+    "fk": "%(table_name)s_%(column_0_name)s_fkey",
+    "pk": "%(table_name)s_pkey",
+}
+
+db_settings = DatabaseSettings()
+metadata = MetaData(naming_convention=DB_NAMING_CONVENTION)  # pyright: ignore
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    metadata = metadata
+
+
+engine = create_async_engine(db_settings.url, echo=True)
+
+sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
