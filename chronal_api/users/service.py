23a7403f1b36ec23585be4ce7033c9dff7f32703
bartosz121 <bmagiera121@gmail.com>
2023-09-23 22:26:14 +0200

    Move exceptions to separate file

--- chronal_api/users/service.py
+++ chronal_api/users/service.py
@@ -2,17 +2,8 @@ from uuid import UUID
 
 from chronal_api.lib.auth import security as auth_security
 from chronal_api.lib.service import Service
-from chronal_api.lib.service.exceptions import ServiceException
 
-from . import models, repository, schemas
-
-
-class EmailAlreadyExists(ServiceException):
-    ...
-
-
-class UserNotFound(ServiceException):
-    ...
+from . import exceptions, models, repository, schemas
 
 
 class UserService(Service[models.User, UUID]):
@@ -25,12 +16,12 @@ class UserService(Service[models.User, UUID]):
 
         email_exists = await self.repository.email_exists(data.email)
         if email_exists:
-            raise EmailAlreadyExists()
+            raise exceptions.EmailAlreadyExists()
 
         return await self.repository.create_user(data.email, hashed_password)
 
     async def get_by_email(self, email: str) -> models.User:
         user = await self.repository.get_by_email(email)
         if user is None:
-            raise UserNotFound()
+            raise exceptions.UserNotFound()
         return user
