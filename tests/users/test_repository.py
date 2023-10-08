e0e2aa1feddcde3fcbf37c2dcbfd737cdb2b91db
bartosz121 <bmagiera121@gmail.com>
2023-10-08 22:23:18 +0200

    Update users tests

--- tests/users/test_repository.py
+++ tests/users/test_repository.py
@@ -7,42 +7,42 @@ from chronal_api.users import repository as users_repository
 
 
 @pytest.fixture
-def user_repository() -> users_repository.UserRepository:
+def user_repository_unit() -> users_repository.UserRepository:
     return users_repository.UserRepository(mock.AsyncMock())
 
 
 @mock.patch.object(users_repository.UserRepository, "_expunge")
 async def test_get_by_email(
     _: mock.AsyncMock,
-    user_repository: users_repository.UserRepository,
+    user_repository_unit: users_repository.UserRepository,
 ):
     execute_return = mock.Mock()
-    user_repository.session.execute.return_value = execute_return
+    user_repository_unit.session.execute.return_value = execute_return
     user = mock.MagicMock(spec=users_models.User)
     execute_return.scalar_one_or_none.return_value = user
 
-    u = await user_repository.get_by_email("email")
+    u = await user_repository_unit.get_by_email("email")
     assert u
 
 
 @mock.patch.object(users_repository.UserRepository, "_expunge")
 async def test_get_by_email_returns_none_if_not_found(
     _: mock.AsyncMock,
-    user_repository: users_repository.UserRepository,
+    user_repository_unit: users_repository.UserRepository,
 ):
     execute_return = mock.Mock()
-    user_repository.session.execute.return_value = execute_return
+    user_repository_unit.session.execute.return_value = execute_return
     execute_return.scalar_one_or_none.return_value = None
 
-    u = await user_repository.get_by_email("email")
+    u = await user_repository_unit.get_by_email("email")
     assert u is None
 
 
 @mock.patch.object(users_repository.UserRepository, "exists")
 async def test_email_exists(
-    mock_exists: mock.AsyncMock, user_repository: users_repository.UserRepository
+    mock_exists: mock.AsyncMock, user_repository_unit: users_repository.UserRepository
 ):
-    await user_repository.email_exists("email")
+    await user_repository_unit.email_exists("email")
     mock_exists.assert_called_once_with(email="email")
 
 
@@ -51,12 +51,12 @@ async def test_email_exists(
 async def test_create_user(
     mock_create: mock.AsyncMock,
     mock_user: mock.MagicMock,
-    user_repository: users_repository.UserRepository,
+    user_repository_unit: users_repository.UserRepository,
 ):
     user = mock.Mock()
     mock_user.return_value = user
 
-    r = await user_repository.create_user("email", "hashed_password")
+    r = await user_repository_unit.create_user("email", "hashed_password")
 
     mock_user.assert_called_once_with(email="email", hashed_password="hashed_password")
     mock_create.assert_called_once_with(user)
